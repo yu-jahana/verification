90c2438e252e909045f7f88028ff8dfa68c95ff6
user <user@MOVmac005.local>
2017-11-01 13:44:49 +0900

    こみっと

--- assets/sp/js/Sw.js
+++ assets/sp/js/Sw.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     console.log('ok');
         navigator.serviceWorker
-             .register('https://yu-jahana.github.io/verification/assets/sp/js/ServiceWorker.js')
+             .register('https://yu-jahana.github.io/verification/assets/sp/js/ServiceWorker')
              .then(() => { console.log('Service Worker Registered'); });
 } else {
     console.log('ng');
