f6c659f6afe5a8c0bd5937175ea13f01ef53d254
user <user@MOVmac005.local>
2017-11-01 11:59:11 +0900

    こみっと

--- assets/sp/js/Sw.js
+++ assets/sp/js/Sw.js
@@ -1,8 +1,10 @@
 if ('serviceWorker' in navigator) {
     console.log('ok');
+/*
         navigator.serviceWorker
              .register('./ServiceWorker')
              .then(() => { console.log('Service Worker Registered'); });
+*/
 } else {
     console.log('ng');
 }
